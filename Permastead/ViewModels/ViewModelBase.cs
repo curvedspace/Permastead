8f9d17724af6b89d79dcbaea76da3225a2de4df1
Shayne White <shayne.s.white@outlook.com>
2023-06-26 19:36:53 -0300

    Add project files.

---
+++ Permastead/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Permastead.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ Permastead/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace Permastead.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
