ece86b277199fbd178fd3c79581e16e5a29d3538
Shayne White <shayne.s.white@outlook.com>
2023-06-27 18:45:45 -0300

    Add guild unit test

---
+++ UnitTests/Models/GuildTest.cs
@@ -0,0 +1,25 @@
+﻿
+namespace UnitTests.Models
+{
+    public class GuildTest
+    {
+        [Fact]
+        public void TestAddPlant()
+        {
+            var guild = new Guild
+            {
+                 Id = 1,
+                 Code = "ABC",
+                 Description = "My Guild"
+            };
+
+            Assert.Empty(guild.Plants);
+
+            var plant = new Plant { Id = 1, Code = "PEA", Description = "Stanhope Pea" };
+
+            guild.AddPlant(plant);
+            Assert.NotEmpty(guild.Plants);
+
+        }
+    }
+}
