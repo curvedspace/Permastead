46aee079f10b13f731af624fdc9ce102a58710b5
curvedspace <shayne.white@gmail.com>
2023-09-09 16:51:16 -0300

    Working on Mac publishing

--- Publish/Mac/MacOs.sh
+++ Publish/Mac/MacOs.sh
@@ -17,7 +17,7 @@ limitations under the License.
 
 ORIGIN="$(pwd)"
 cd "../.."
-dotnet publish -r osx-x64 -c Release -p:SelfContained=True -o bin/Release/net6.0/publishMac
+dotnet publish -r osx-x64 -c Release -p:SelfContained=True -p:UseAppHost=true -o bin/Release/net6.0/publishMac
 cd "$ORIGIN"
 
 APP_NAME="Permastead.app"
