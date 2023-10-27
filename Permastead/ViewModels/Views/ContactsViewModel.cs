e6fd8717b414eee4706df077e84c6a16c617d75e
ssw <shayne.white@gmail.com>
2023-10-27 10:27:36 -0300

    Add header to contacts view

--- Permastead/ViewModels/Views/ContactsViewModel.cs
+++ Permastead/ViewModels/Views/ContactsViewModel.cs
@@ -1,14 +1,20 @@
+using System;
 using System.Collections.ObjectModel;
 using Avalonia.Controls;
 using Avalonia.Controls.Models.TreeDataGrid;
 using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Models;
 
 namespace Permastead.ViewModels.Views;
 
-public class ContactsViewModel : ViewModelBase
+public partial class ContactsViewModel : ViewModelBase
 {
     private ObservableCollection<Person> _people;
+    
+    [ObservableProperty] 
+    private long _peopleCount;
 
     public FlatTreeDataGridSource<Person> PersonSource { get; }
     
@@ -29,8 +35,18 @@ public class ContactsViewModel : ViewModelBase
         
         // PersonSource.Columns.Add(new TextColumn<Person, string>("Last Name", x => x.LastName));
         // PersonSource.Columns.Add(new TextColumn<Person, string>("First Name", x => x.FirstName));
+
+        PeopleCount = _people.Count;
+    }
+    
+    [RelayCommand]
+    // The method that will be executed when the command is invoked
+    private void SaveEvent()
+    {
+        //if there is a comment, save it.
+
         
-        
+
     }
     
 }
