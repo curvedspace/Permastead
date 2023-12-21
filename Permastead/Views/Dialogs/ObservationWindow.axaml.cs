863311f478df23da6356e69d7a3b04e5c14c2b53
Shayne White <shayne.s.white@outlook.com>
2023-12-20 20:48:44 -0400

    Start of an Observation editing window

---
+++ Permastead/Views/Dialogs/ObservationWindow.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Permastead.Views.Dialogs;
+
+public partial class ObservationWindow : Window
+{
+    public ObservationWindow()
+    {
+        InitializeComponent();
+    }
+}
--- Permastead/Views/Views/ObservationsView.axaml.cs
+++ Permastead/Views/Views/ObservationsView.axaml.cs
@@ -1,7 +1,11 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using Models;
 using Permastead.ViewModels.Views;
+using Permastead.Views.Dialogs;
 
 namespace Permastead.Views.Views;
 
@@ -16,5 +20,18 @@ public partial class ObservationsView : UserControl
     {
         AvaloniaXamlLoader.Load(this);
     }
-    
+
+    private void ObservationsList_OnDoubleTapped(object? sender, TappedEventArgs e)
+    {
+        //get the selected row in the list
+        var currentObs = sender as ListBox;
+        var obs = currentObs.SelectedValue as Observation;
+        
+        var obsWindow = new ObservationWindow();
+
+        obsWindow.Topmost = true;
+        obsWindow.Width = 250;
+        obsWindow.Height = 200;
+        obsWindow.Show();
+    }
 }
