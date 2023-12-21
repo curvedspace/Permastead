4ca379a6d3ad87033ef1b84b848db6efd16cc4f0
ssw <shayne.white@gmail.com>
2023-12-21 13:23:04 -0400

    Trying to add weather to Gaia start up.

--- Permastead/ViewModels/Views/GaiaViewModel.cs
+++ Permastead/ViewModels/Views/GaiaViewModel.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Models;
+using Services;
 
 namespace Permastead.ViewModels.Views;
 
@@ -18,6 +21,8 @@ public partial class GaiaViewModel : ViewModelBase
     private string _response = string.Empty;
 
     private Services.GaiaService _gaia;
+    
+    [ObservableProperty] private string _weatherForecast = "Weather Unknown";
 
     [ObservableProperty]
     private ObservableCollection<RequestResponse> _RequestResponses = new ObservableCollection<RequestResponse>();
@@ -27,9 +32,62 @@ public partial class GaiaViewModel : ViewModelBase
     {
         _gaia = new Services.GaiaService();
 
-        Response = _gaia.GetResponse("Hello");
+        //Response = _gaia.GetResponse("Hello");
+        
+        try
+        {
+            var t = new Task(GetWeatherAsync);
+            t.Start();
+            t.Wait();
+
+            if (string.IsNullOrEmpty(WeatherForecast))
+            {
+                RequestResponses.Add(new RequestResponse() { Request = "Hello", Response = "Hello, I am Gaia." });
+            }
+            else
+            {
+                RequestResponses.Add(new RequestResponse() { Request = "The weather please?", Response = WeatherForecast });
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
 
     }  
+    
+    async void GetWeatherAsync()
+    {
+        var ws = new Services.WeatherService();
+        //var city = new City("Halifax", "Canada", 44.6475, -63.5906, "CA");
+            
+        //get location from settings
+        var location = SettingsService.GetSettingsForCode("LOC");
+        var ctry = SettingsService.GetSettingsForCode("CTRY");
+
+        if (!string.IsNullOrEmpty(location) && !string.IsNullOrEmpty(ctry))
+        {
+            var city = new City(location, ctry, 0, 0, "");
+
+            try
+            {
+                var results = await ws.UpdateWeather(city);
+                WeatherForecast = "Current Weather for " + city.Name + ", " + city.Country + ": " + results.WeatherStateAlias + ", Temperature: " + results.Temperature + ", Humidity: " + results.Humidity;
+                Console.WriteLine(WeatherForecast);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                WeatherForecast = "Unable to get weather data.";
+            }
+        }
+        else
+        {
+            WeatherForecast = "";
+        }
+            
+    }
 
     [RelayCommand]
     private void SendRequest()
@@ -56,6 +114,8 @@ public partial class GaiaViewModel : ViewModelBase
     {
         return Response != null;
     }
+    
+    
     
 }
 
