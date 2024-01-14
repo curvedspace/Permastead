e9a83ddd1570035cdeefa313decae567ec838d72
ssw <shayne.white@gmail.com>
2024-01-14 11:06:30 -0400

    Add status bar to app

--- Permastead/ViewModels/MainWindowViewModel.cs
+++ Permastead/ViewModels/MainWindowViewModel.cs
@@ -37,60 +37,70 @@ namespace Permastead.ViewModels
         private void OpenHomeView()
         {
             CurrentView = Views[0];
+            CurrentViewName = "Home";
         }
         
         [RelayCommand]
         private void OpenDashboardView()
         {
             CurrentView = Views[1];
+            CurrentViewName = "Dashboard";
         }
 
         [RelayCommand]
         private void OpenObservationView()
         {
             CurrentView = Views[2];
+            CurrentViewName = "Observations";
         }
         
         [RelayCommand]
         private void OpenToDoView()
         {
             CurrentView = Views[3];
+            CurrentViewName = "Actions";
         }
         
         [RelayCommand]
         private void OpenEventsView()
         {
             CurrentView = Views[4];
+            CurrentViewName = "Events";
         }
         
         [RelayCommand]
         private void OpenInventoryView()
         {
             CurrentView = Views[5];
+            CurrentViewName = "Inventory";
         }
         
         [RelayCommand]
         private void OpenSeedsView()
         {
             CurrentView = Views[6];
+            CurrentViewName = "Seeds";
         }
         
         [RelayCommand]
         private void OpenPlantingsView()
         {
             CurrentView = Views[7];
+            CurrentViewName = "Plantings";
         }
         
         [RelayCommand]
         private void OpenPeopleView()
         {
             CurrentView = Views[8];
+            CurrentViewName = "People";
         }
         
         [RelayCommand]
         private void OpenSettingsView()
         {
             CurrentView = Views[9];
+            CurrentViewName = "Settings";
         }
 
         public MainWindowViewModel()
