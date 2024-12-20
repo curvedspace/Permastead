222255d983694317a97db8878b53cd840e96bd5d
ssw <shayne.white@gmail.com>
2024-12-20 10:50:18 -0400

    Add shell for Food PreservationView.axaml

--- Permastead/ViewModels/Views/PreservationViewModel.cs
+++ Permastead/ViewModels/Views/PreservationViewModel.cs
@@ -1,6 +1,47 @@
+using Avalonia.Controls;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Models;
+using Permastead.ViewModels.Dialogs;
+using Permastead.Views.Dialogs;
+
 namespace Permastead.ViewModels.Views;
 
-public class PreservationViewModel : ViewModelBase
+public partial class PreservationViewModel : ViewModelBase
 {
+    [ObservableProperty] 
+    private bool _currentOnly = true;
+
+    [ObservableProperty]
+    private long _preservationCount;
+
+    [ObservableProperty]
+    private FoodPreservation _currentItem;
     
+    [ObservableProperty] 
+    private bool _showObservations;
+    
+    [ObservableProperty]
+    private string _searchText = "";
+    
+    public FlatTreeDataGridSource<FoodPreservation> PreservationSource { get; set; }
+    
+    [RelayCommand]
+    private void RefreshData()
+    {
+        
+    }
+    
+    [RelayCommand]
+    private void ClearSearch()
+    {
+        SearchText = "";
+        //RefreshDataOnly(SearchText);
+    }
+    
+    [RelayCommand]
+    public void EditRecord()
+    {
+        
+    }
 }
--- Permastead/Views/Views/PreservationView.axaml.cs
+++ Permastead/Views/Views/PreservationView.axaml.cs
@@ -1,5 +1,6 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 
 namespace Permastead.Views.Views;
@@ -9,5 +10,20 @@ public partial class PreservationView : UserControl
     public PreservationView()
     {
         InitializeComponent();
+    }
+
+    private void PreservationGrid_OnTapped(object? sender, TappedEventArgs e)
+    {
+        
+    }
+
+    private void PreservationGrid_OnDoubleTapped(object? sender, TappedEventArgs e)
+    {
+        
+    }
+
+    private void SearchBox_OnKeyDown(object? sender, KeyEventArgs e)
+    {
+        
     }
 }
