415c73e02d428b6d4288409e133305a09afe9c03
ssw <shayne.white@gmail.com>
2025-01-03 19:28:59 -0400

    Add constructor for AnimalType.cs

--- Models/AnimalType.cs
+++ Models/AnimalType.cs
@@ -2,5 +2,22 @@ namespace Models;
 
 public class AnimalType : CodeTable
 {
-    
+    public Person Author { get; set; }
+
+    public long AuthorId => this.Author.Id;
+
+
+    public AnimalType()
+    {
+        this.CreationDate = DateTime.Now;
+        this.StartDate = DateTime.Today;
+        this.EndDate = DateTime.MaxValue;
+        this.Author = Person.Anonymous();
+    }
+
+    public AnimalType(long id) : base()
+    {
+        this.Id = id;
+    }
+
 }
