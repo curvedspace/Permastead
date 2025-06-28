311c66a818a46eba9eb69a1311da55570bf7b932
ssw <shayne.white@gmail.com>
2025-06-28 12:31:45 -0300

    Beginning of adding images for plants

--- Permastead/Views/Views/PlantsView.axaml.cs
+++ Permastead/Views/Views/PlantsView.axaml.cs
@@ -1,9 +1,11 @@
 using System;
+using System.IO;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using Avalonia.Platform.Storage;
 using Models;
 using Permastead.ViewModels.Dialogs;
 using Permastead.ViewModels.Views;
@@ -108,5 +110,28 @@ public partial class PlantsView : UserControl
     private void TextBox_OnTextChanged(object? sender, TextChangedEventArgs e)
     {
         
+    }
+
+    private async void MenuItemAddImage_OnClick(object? sender, RoutedEventArgs e)
+    {
+        // Get top level from the current control. Alternatively, you can use Window reference instead.
+        var topLevel = TopLevel.GetTopLevel(this);
+
+        // Start async operation to open the dialog.
+        var files = await topLevel!.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Open Image File",
+            SuggestedFileName = "*.png",
+            AllowMultiple = false
+        });
+
+        if (files.Count >= 1)
+        {
+            // Open reading stream from the first file.
+            await using var stream = await files[0].OpenReadAsync();
+            using var streamReader = new StreamReader(stream);
+            // Reads all the content of file as a text.
+            var fileContent = await streamReader.ReadToEndAsync();
+        }
     }
 }
