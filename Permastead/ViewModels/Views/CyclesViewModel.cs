64f3426349de443175f4f2451e8fc8d652aafb32
ssw <shayne.white@gmail.com>
2025-11-10 16:41:01 -0400

    Small changes for Cycles view

--- Permastead/ViewModels/Views/CyclesViewModel.cs
+++ Permastead/ViewModels/Views/CyclesViewModel.cs
@@ -1,12 +1,24 @@
+using System.Collections.Generic;
 using CommunityToolkit.Mvvm.Input;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView.Extensions;
 
 namespace Permastead.ViewModels.Views;
 
 public partial class  CyclesViewModel : ViewModelBase
 {
+    public double Value { get; set; } = 30;
+    
+    public IEnumerable<ISeries> Series { get; set; }
+    
     [RelayCommand]
     private void RefreshData()
     {
-        //
+        Series = GaugeGenerator.BuildSolidGauge();
+    }
+
+    public CyclesViewModel()
+    {
+        RefreshData();
     }
 }
