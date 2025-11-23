20bc325caba6403d6bf23fa6125d8d9bb4f1e515
Shayne White <shayne.s.white@outlook.com>
2025-11-23 16:36:59 -0400

    Allow plants to be edited in Finder view

--- Permastead/Views/Views/FinderView.axaml.cs
+++ Permastead/Views/Views/FinderView.axaml.cs
@@ -1,8 +1,13 @@
+using System.Runtime.InteropServices.JavaScript;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using Models;
+using Permastead.ViewModels.Dialogs;
 using Permastead.ViewModels.Views;
+using Permastead.Views.Dialogs;
+using Services;
 
 namespace Permastead.Views.Views;
 
@@ -41,6 +46,36 @@ public partial class FinderView : UserControl
 
     private void SearchResultsGrid_OnDoubleTapped(object? sender, TappedEventArgs e)
     {
+        var tree = this.FindControl<TreeDataGrid>("SearchResultsGrid");
         
+        var current = sender as TreeDataGrid;
+        if (current != null)
+        {
+            var aRecord = (SearchResult)current.RowSelection!.SelectedItem;
+
+            if (aRecord != null)
+            {
+                switch (aRecord.Entity.Name)
+                {
+                   case "Plant" :
+                       var plantWindow = new PlantWindow();
+                       var plant = PlantService.GetPlantFromId(AppSession.ServiceMode, aRecord.Entity.Id);
+                       var plantWindowViewModel = new PlantWindowViewModel();
+                       plantWindowViewModel.Plant = plant;
+                       plantWindow.DataContext = plantWindowViewModel;
+        
+                       plantWindow.Topmost = true;
+                       plantWindow.Width = 900;
+                       plantWindow.Height = 700;
+                       plantWindow.Opacity = 0.95;
+                       plantWindow.Title = "Edit Plant";
+                       plantWindow.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+                
+                       plantWindow.Show();
+                       break;
+                       
+                }
+            }
+        }
     }
 }
