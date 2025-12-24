7c3282610528161746958cba6def1fcec844fe88
ssw <shayne.white@gmail.com>
2025-12-24 12:56:07 -0400

    Small start on the Data Admin view

--- Permastead/ViewModels/Views/DataViewModel.cs
+++ Permastead/ViewModels/Views/DataViewModel.cs
@@ -1,6 +1,14 @@
+using CommunityToolkit.Mvvm.Input;
+
 namespace Permastead.ViewModels.Views;
 
 public partial class DataViewModel : ViewModelBase
 {
+
+    [RelayCommand]
+    private void RefreshData()
+    {
+        
+    }
     
 }
