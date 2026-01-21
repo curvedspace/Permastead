8031860458cfb3175e8fe9def2c3386c7886f9a1
ssw <shayne.white@gmail.com>
2026-01-21 12:04:23 -0400

    Added a try/catch around the main procedure

--- Permastead.Desktop/Program.cs
+++ Permastead.Desktop/Program.cs
@@ -18,39 +18,47 @@ class Program
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
     {
-        var appBuilder = AppBuilder.Configure<App>()
-            .UsePlatformDetect()
-            .WithInterFont()
-            .LogToTrace();
-        
-        //create logging 
-        string logFilename = string.Empty;
-        var userFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-        var isWindows = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
-            
-        if (isWindows)
+        try
         {
-            logFilename = userFolder + @"\.config\permastead\log.txt";
-        }
-        else
-        {
-            //linux or macos
-            logFilename = userFolder + @"/.config/permastead/log.txt";
-        }
             
-        Log.Logger = new LoggerConfiguration()
-            .Enrich.FromLogContext()
-            .MinimumLevel.Information()
-            .WriteTo.Console()
-            .WriteTo.File(logFilename,
-                rollingInterval: RollingInterval.Day,
-                rollOnFileSizeLimit: true)
-            .CreateLogger();
+            var appBuilder = AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .WithInterFont()
+                .LogToTrace();
             
-        Log.Information("Starting Permastead");
+            //create logging 
+            string logFilename = string.Empty;
+            var userFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            var isWindows = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+                
+            if (isWindows)
+            {
+                logFilename = userFolder + @"\.config\permastead\log.txt";
+            }
+            else
+            {
+                //linux or macos
+                logFilename = userFolder + @"/.config/permastead/log.txt";
+            }
+                
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .WriteTo.File(logFilename,
+                    rollingInterval: RollingInterval.Day,
+                    rollOnFileSizeLimit: true)
+                .CreateLogger();
+                
+            Log.Information("Starting Permastead");
 
-        return appBuilder;
+            return appBuilder;
+            
+        }
+        finally
+        {
+            
+        }
+        
     }
-        
-
 }
