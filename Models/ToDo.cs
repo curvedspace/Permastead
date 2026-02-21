a882d4224a67e62c4d2493e168d9f42b62bb6597
ssw <shayne.white@gmail.com>
2026-02-21 12:12:04 -0400

    Add a day to todos due dates

--- Models/ToDo.cs
+++ Models/ToDo.cs
@@ -18,7 +18,7 @@ public class ToDo
         
     public string DisplayDueDate => this.DueDate.ToShortDateString();
 
-    public long DaysUntilDue => new TimeSpan(DueDate.Ticks - DateTime.Now.Ticks).Days;
+    public long DaysUntilDue => new TimeSpan(DueDate.Ticks - DateTime.Now.Ticks).Days + 1;
 
     public int PercentDone { get; set; }
 
