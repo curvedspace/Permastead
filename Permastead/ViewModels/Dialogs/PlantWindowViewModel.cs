a5fafc0344078fe1fc93a6135bc1845fad163796
ssw <shayne.white@gmail.com>
2026-07-07 10:13:17 -0300

    Check for nulls when saving plant records.

--- Permastead/ViewModels/Dialogs/PlantWindowViewModel.cs
+++ Permastead/ViewModels/Dialogs/PlantWindowViewModel.cs
@@ -75,12 +75,17 @@ public partial class PlantWindowViewModel : ViewModelBase
 
         _plant.TagList.Clear();
 
-        foreach (var tagData in SelectedItems)
+        if (SelectedItems != null)
         {
-            _plant.TagList.Add(tagData.TagText);
-        }
             
-        _plant.SyncTags();
+            foreach (var tagData in SelectedItems)
+            {
+                _plant.TagList.Add(tagData.TagText);
+            }
+                
+            _plant.SyncTags();
+        
+        }
         rtnValue = Services.PlantService.CommitRecord(AppSession.ServiceMode, _plant);
         
         OnPropertyChanged(nameof(_plant));
